3808930910221361e8e6805adf516af0cd1fd052
chrisbaidoo <chris.baidoo@smartpension.co.uk>
2019-04-07 19:57:04 +0100

    Display sum in the window

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 function calc(){
-  var a = parseInt(document.querySelector("#value1").value);
-  var b = parseInt(document.querySelector("#value2").value);
-  var operator = document.querySelector("#operator").value;
+  var val1 = parseInt(document.querySelector("#value1").value); //get the value from 1st Value. change to number
+  var val2  = parseInt(document.querySelector("#value2").value); //get the value from 2nd Value. change to number
+  var operator = document.querySelector("#operator").value; //get the value from the operator
   var calculate;
 
-  if (operator == 'add') {
-    calculate = a + b;
-  } else if (operator == 'sub') {
-    calculate = a - b;
-  } else if (operator == 'div') {
-    calculate = a / b;
-  }else if (operator == 'mul') {
-    calculate = a * b;
+  if (operator == 'add') { // Check if the operator = 'add' then do an addition
+    calculate = val1 + val2 ;
+  } else if (operator == 'sub') { // Check if the operator = 'sub' then doval1subtraction
+    calculate = val1 - val2 ;
+  } else if (operator == 'div') { // Check if the operator = 'div' then doval1division
+    calculate = val1 / val2 ;
+  }else if (operator == 'mul') { // Check if the operator = 'mul' then doval1multiplication
+    calculate = val1 * val2 ;
   }
 
-  console.log(calculate);
+  // document.getElementById("btn").addEventListener("click", calc, false);
+
+
+  document.querySelector("#result").innerHTML = calculate;
 
 }
